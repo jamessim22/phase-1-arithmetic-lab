5b8114830d42921091b63f46b3a68396055a7e8e
James Simeon <jamessimeon60@gmail.com>
2023-08-17 10:17:16 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+//write your code here
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 39;
+let num4= 5;
+
+let multiply = num1 * num2;
+
+let mod = num3 % num4;
+
+let max = Math.max(4,10,13,20);
+
+let random = 5
+random = math.floor(math.random() * 10);
+
+
